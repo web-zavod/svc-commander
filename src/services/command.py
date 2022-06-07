7b1c07f05af882e49a6d2c4b4f54a0df28e1e32d
Wanderer47 <strannek47@gmail.com>
2022-06-07 11:40:06 +0300

    Add ReplyMessage response

--- src/services/command.py
+++ src/services/command.py
@@ -1,6 +1,7 @@
+import re
 from botapi.command.v1.command_service_pb2 import ReplyMessage
 from botapi.command.v1.command_service_pb2_grpc import CommandServiceServicer
 
 class CommandService(CommandServiceServicer):
     async def GetReply(self, request, context):
-        return ReplyMessage()
+        return ReplyMessage(user_id= request.user_id, text= request.text)
