eca1e090e0969635a63fc214aa5f5484c105831b
Wanderer47 <strannek47@gmail.com>
2022-06-07 22:48:23 +0300

    Add type hint at CommandService

--- src/services/command.py
+++ src/services/command.py
@@ -1,7 +1,6 @@
-import re
-from botapi.command.v1.command_service_pb2 import ReplyMessage
+from botapi.command.v1.command_service_pb2 import ReplyMessage, IncomingMessage
 from botapi.command.v1.command_service_pb2_grpc import CommandServiceServicer
 
 class CommandService(CommandServiceServicer):
-    async def GetReply(self, request, context):
+    async def GetReply(self, request: IncomingMessage, context) -> ReplyMessage:
         return ReplyMessage(user_id= request.user_id, text= request.text)
