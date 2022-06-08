dd6e2d421ff436fb7fd28eff1b90f6828b0380c7
Wanderer47 <strannek47@gmail.com>
2022-06-08 18:24:20 +0300

    Fix code design

--- src/services/command.py
+++ src/services/command.py
@@ -3,4 +3,4 @@ from botapi.command.v1.command_service_pb2_grpc import CommandServiceServicer
 
 class CommandService(CommandServiceServicer):
     async def GetReply(self, request: IncomingMessage, context) -> ReplyMessage:
-        return ReplyMessage(user_id= request.user_id, text= request.text)
+        return ReplyMessage(user_id=request.user_id, text=request.text)
