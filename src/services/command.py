ccdbf911439492f6aa764f0cfece16d9372619b1
Wanderer47 <58740918+Wanderer47@users.noreply.github.com>
2022-06-27 13:00:22 +0300

    Revert "Fix dependencies (#6)"
    This reverts commit cd909afaaa10e41d72491748c4a9ce3aed0e19fe.

--- src/services/command.py
+++ src/services/command.py
@@ -1,10 +1,6 @@
-import logging
 from botapi.command.v1.command_service_pb2 import ReplyMessage, IncomingMessage
 from botapi.command.v1.command_service_pb2_grpc import CommandServiceServicer
 
-logger = logging.getLogger(__name__)
-
 class CommandService(CommandServiceServicer):
     async def GetReply(self, request: IncomingMessage, context) -> ReplyMessage:
-        logger.debug(f"Receive command: user_id={request.user_id}, text={request.text}")
         return ReplyMessage(user_id=request.user_id, text=request.text)
