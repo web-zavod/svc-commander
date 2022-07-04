6e1005aa906c18e5033c29b7b836395cc1cb32a8
Wanderer47 <strannek47@gmail.com>
2022-07-04 16:25:20 +0300

    Bug settings

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,16 @@
 from typing import Optional
 
-from pydantic_settings import BaseSettingsModel
+from pydantic_settings import BaseSettingsModel, load_settings
 
 class TransportSettings(BaseSettingsModel):
     port: Optional[str] = "[::]:5000"
 
 class AppSettings(BaseSettingsModel):
     transport: TransportSettings
-    
+
     class Config:
         env_prefix = 'APP'
+
+    @classmethod
+    def load(cls):
+        return load_settings(cls=cls, load_env=True)
