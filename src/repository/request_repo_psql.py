964f043607ee6d8e8afee8f7f82e67651f26e74a
Wanderer47 <strannek47@gmail.com>
2022-09-16 19:59:31 +0300

    Add list expenses method

--- src/repository/request_repo_psql.py
+++ src/repository/request_repo_psql.py
@@ -7,11 +7,11 @@ from models import Expenses
 async def get_many_by_id(user_id: int) -> AsyncGenerator[Expenses, None]:
     await application.db_connect()
     cursor = await application.get_db_cursor()
-    await cursor.execute(f"SELECT ex.amount, ex.created, ca.name FROM expense AS ex, category AS ca WHERE ex.category_id=ca.id AND owner={user_id};")
+    await cursor.execute(f"SELECT ex.amount, ex.created, ca.name FROM expense AS ex, category AS ca WHERE ex.category_id=ca.id AND owner=%s;", (user_id, ))
 
     data: list[tuple] = await cursor.fetchall()
-    #print('Debug ---> ', data)
-    #cursor.close()
+
+    await application.db_disconnect()
 
     for record in data:
         yield Expenses.from_row(record)
--- src/tasks.py
+++ src/tasks.py
@@ -1,15 +1,12 @@
-from cgitb import text
 from repository import request_repo_psql
-from models import Expenses
 
 async def get_expenses(user_id: int):
     expenses: list[str] = []
 
     generator = request_repo_psql.get_many_by_id(user_id)
 
-    async for expens in generator:    
-        #print('debug ---> ', expens.get_info())
-        expenses.append(expens.get_info())
+    async for expense in generator:    
+        expenses.append(expense.get_info())
 
     text = 'Your expenses for today:[newline] {0}'.format("[newline]".join(expenses))
     
